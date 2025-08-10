fb8e4b1e12a3ffdba0dc6c3d4d97a390b1081932
Wajiha Arif <hackrgirl787@gmail.com>
2025-08-10 14:36:04 +0500

    New commit

--- Defualt_function_argument.cpp
+++ Defualt_function_argument.cpp
@@ -3,7 +3,7 @@ using namespace std;
   
   void show(int x , float y )
   { 
-     cout << "value of x : " << x << endl ; 
+     cout << "\nvalue of x : " << x << endl ; 
      cout << "value of y : " << y << endl ; 
   }
 //   In this case i didnt set any value for the parameters by default bu in next fuunction m gonna set them 
@@ -14,7 +14,23 @@ using namespace std;
       cout << "the value of x is : " << x << endl ;  
       cout << "the value of y is : " << y << endl ;   
   }
+
+
+//    Now global variable : Basically we can use it any where in our code as its global 
+         int var = 100 ; 
+     
+     
 int main(){
+
+    //  Local variable as it can only use within this main function 
+        
+        int  var = 10 ; 
+         
+         cout << "The var is equal to " << var ; 
+        //    this will print the local variables value 
+         
+         cout << "\nThe var is equal to  " << ::var ; 
+        //    This will give value of global variable as we used :: in this  
      
      show(2, 4.5) ; 
       show2() ; 
