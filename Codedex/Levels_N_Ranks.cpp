c4bda3a335243cd99d6b9613bfb533ac3f1b3385
Wajiha Arif <hackrgirl787@gmail.com>
2025-08-15 00:50:47 +0500

    New commit

---
+++ Codedex/Levels_N_Ranks.cpp
@@ -0,0 +1,34 @@
+ 
+  // Arithmatic operator  (+ , - , / , * , % )
+  // Relational operator (+= , -= , != , == , > , >= , < , <= ) 
+
+  #include <iostream>
+using namespace std;
+
+int main() {
+   
+   int level ; 
+
+   cout << "Tell your level to know in which league you are currently : " ; 
+   cin >> level ; 
+
+   if(level <= 5)
+   { 
+     cout << "\nBronze" ; 
+   }
+   else if(level >= 10) 
+   { 
+      cout << "\nSilver" ; 
+   }
+   else if(level >= 15)
+   {
+      cout << "\nPlatinum" ; 
+   }
+   else 
+   { 
+     cout << "\nDiamond " ; 
+   }
+
+   return 0 ; 
+}
+
