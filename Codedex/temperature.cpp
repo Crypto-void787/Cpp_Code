cc8fcad442d0035540a94702e1598352e0cb695d
Wajiha Arif <hackrgirl787@gmail.com>
2025-08-14 23:11:05 +0500

    New commit

---
+++ Codedex/temperature.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std ; 
+
+int main() {
+  // Write code here
+
+  double celsius ; 
+  double Fahrenheit ; 
+
+  celsius = (Fahrenheit - 32) / 1.8 ; 
+
+  cout << "\nwrite the temperature in fahrenheit to convert into celsius : " ; 
+  cin >> Fahrenheit ; 
+   
+   cout << "\nThe temperature in celsius is : " << celsius << endl ; 
+  
+}
