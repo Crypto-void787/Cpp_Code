9008f9e9b4713a7c3371c2e45151bcbbec6f74d7
Wajiha Arif <hackrgirl787@gmail.com>
2025-08-16 18:54:34 +0500

    New commiit

--- Codedex/FizzBuzz.cpp
+++ Codedex/FizzBuzz.cpp
@@ -5,29 +5,29 @@
 // Here's the tricky part: For multiples of 3 and 5, print "FizzBuzz".
 
 #include<iostream>
-using namespace std; 
-int main()
-{ 
-     for(int i = 1 ; i <=100 ; i++)
-     {
-         if( i % 3 == 0 && i % 5 == 0)  //if i is multiple of both 3  and 5 give fizz buzz
-         {
-             cout << "FizzBuzz" << endl ; 
-         }
-         else if( i % 3 == 0)  // if i is only multiple of 3 give fizz
-         { 
-            cout << "Fizz" << endl ; 
-         }
-         else if( i % 5 == 0) // if i is only multiple of 5 give buzz
-         {
-             cout << "Buzz" << endl ; 
-         }
-         else   // else just print i 
-         {
-             cout << i << endl ; 
-         }
-         
-     }
+using namespace std ; 
 
+int main()
+{
+     for(int i = 0 ; i <= 100 ; i++)
+     {
+         if(i % 3 == 0 && i % 5 == 0)  //if the number is multiple of both 3 and 5 
+         {
+            cout << "FizzBuzz" << endl ;
+         }
+         else if(i % 3 == 0)  // if number is only multiple of 3 print fizz
+         {
+            cout << "Fizz" << endl ; 
+          }
+          else if(i % 5 == 0) // if number is multiple of 5 print buzz
+          {
+             cout << "Buzz" << endl ; 
+          }
+          else // else just print the number 
+          {
+             cout << i << endl ; 
+          }
+
+     }
      return 0 ; 
 }
